65cb1a47e17eb36c75421a6cad36a272e6f5eec5
Tun Cham Roeun <chamroeuntun25@outlook.com>
2025-03-31 21:37:03 +0700

    fix `clientInExt` regex

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-05',
-
-  modules: ['nuxt-graphql-client'],
-
+  modules: ['../src/module'],
   runtimeConfig: {
     public: {
       GQL_HOST: 'https://spacex-api-2gl6xp7kua-ue.a.run.app/query'
